57cb89da168082a7a2df7cd9c2a58a7a6fffe4ab
Dmitriy Vishnevskiy <0x000def42@gmail.com>
2020-12-18 23:50:04 +0300

    Setup void controllers

---
+++ apps/web/controllers/autors/ips.rb
@@ -0,0 +1,13 @@
+module Web
+  module Controllers
+    module Autors
+      class Ips
+        include Web::Action
+
+        def call(params)
+          
+        end
+      end
+    end
+  end
+end
---
+++ apps/web/controllers/posts/create.rb
@@ -0,0 +1,13 @@
+module Web
+  module Controllers
+    module Posts
+      class Create
+        include Web::Action
+
+        def call(params)
+          
+        end
+      end
+    end
+  end
+end
