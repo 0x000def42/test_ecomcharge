3d772ec17531330688ee464ed3fd3ae70f3878d5
Dmitriy Vishnevskiy <0x000def42@gmail.com>
2020-12-19 19:11:00 +0300

    Update creating post

--- spec/web/controllers/posts/create_spec.rb
+++ spec/web/controllers/posts/create_spec.rb
@@ -3,9 +3,20 @@ require_relative '../../../../apps/web/controllers/posts/create'
 RSpec.describe Web::Controllers::Posts::Create do
   let(:action) { Web::Controllers::Posts::Create.new }
   let(:params) { {title: "Some title", content: "Some content", login: "Some login", ip: "255:255:255:255"} }
+  let(:result) do
+    {
+      content: "Some content", 
+      ip: "255:255:255:255", 
+      title: "Some title", 
+      user: {
+        login: "Some login"
+      }
+    }
+  end
 
   it "is successful" do
     response = action.call(params)
     expect(response[0]).to be(200)
+    expect(response[2]).to eq(result)
   end
 end
